595fe7a6d0ac8a474893b0993edba8a2d4cd8dff
Joao Andrade <jjadonotenter@gmail.com>
2016-11-01 12:48:55 +0000

    Add test to distinguish player

--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -3,12 +3,24 @@ defmodule GameOfLifeTest do
   doctest GameOfLife
 
   describe "GameOfLife.get_player" do
+    setup context do
+      plays = File.read!("test/fixtures/play.txt")
+      [
+        player_w: plays,
+        player_b: Regex.replace(~r/^.*\n/, plays, "b\n")
+      ]
+    end
+
     test "should read no player if no plays" do
       assert GameOfLife.get_player == nil
     end
 
-    test "should read a player if there are plays", context do
-      assert GameOfLife.get_player(context[:plays]) == :w
+    test "should read player w", context do
+      assert GameOfLife.get_player(context[:player_w]) == :w
+    end
+
+    test "should read player b", context do
+      assert GameOfLife.get_player(context[:player_b]) == :b
     end
   end
 end
