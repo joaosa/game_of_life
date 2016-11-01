d79c2a34bcf47dcf7233be64c8ba4b862295911e
Joao Andrade <jjadonotenter@gmail.com>
2016-11-01 19:13:43 +0000

    Make it pass

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -2,10 +2,13 @@ defmodule GameOfLife do
   def get_player do
     nil
   end
-
   def get_player(plays) do
     [head|_] = String.split plays, "\n"
     [player|_] = String.split head, ""
     String.to_atom(player)
+  end
+
+  def get_grid do
+    nil
   end
 end
